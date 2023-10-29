ad2b4e784ec5e2e89b6768f799e3a40d8a5acba2
Allen Febi M A <allenabi2614@gmail.com>
2023-10-29 22:29:05 +0530

    Empty Files

--- riders_paradise/src/App.js
+++ riders_paradise/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function LandingPage() {
+  return (
+    <div className="page">
+      <div className="header"></div>
+      <div className="body"></div>
+      <div className='footer'></div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
