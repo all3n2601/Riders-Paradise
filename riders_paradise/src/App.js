905a2417cbb2d73822d0ed7ceed7b8046c257b65
Allen Febi M A <allenabi2614@gmail.com>
2023-11-03 01:44:43 +0530

    BackGround Model

--- riders_paradise/src/App.js
+++ riders_paradise/src/App.js
@@ -4,6 +4,41 @@ import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
 import ForgotPassword from "./pages/Forgotpassword";
 import HomePage from "./pages/About";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import "../src/styles/styles.css";
+
+function BikeShow() {
+  return (
+    <>
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+
+      <color args={[0, 0, 0]} attach="background" />
+
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+    </>
+  );
+}
 
 function LandingPage() {
   return (
@@ -23,12 +58,18 @@ function LandingPage() {
             Contact Us
           </Link>
           <Link className="sign" to="/login">
-            SIGN IN
+            Sign In
           </Link>
         </nav>
       </div>
-      <div className="body"></div>
-      <div className="footer"></div>
+      <div className="body">
+        <Suspense fallback={null}>
+          <Canvas shadows>
+            <BikeShow />
+          </Canvas>
+        </Suspense>
+      </div>
+
     </div>
   );
 }
