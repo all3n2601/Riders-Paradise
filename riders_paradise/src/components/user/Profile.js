32e1d5e6898f6b8c4cee68fc45dc7196b454b6f2
varunvenkat22 <varun.21bce7358@vitapstudent.ac.in>
2023-11-15 10:29:57 +0530

    commited profile page

--- riders_paradise/src/components/user/Profile.js
+++ riders_paradise/src/components/user/Profile.js
@@ -1,43 +1,161 @@
-import React from "react";
-import "../../styles/Profile.css";
-function Center({children}) {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100vh',
-        }}
-      >
-        {children}
-      </div>
-    );
-  }
+import React, { useState } from 'react';
+import '../../styles/Profile.css';
 
-function Profile() {
-    return (
-        <Center>
-    
-        <div className="upc">
-            <div className="gradient"></div>
-            <div className="profile-down">
-                <div className="profile-title">User Details</div>
-                <div className="profile-description">
-                <p>Name: Userxyz</p>
-                <p>Emaild: user@gmail.com</p>
-                <p>location: chennai</p>
-                <p>pincode: 600005</p>
 
-                </div>
-                <div className="profile-button"><a href="support@ridersparadise.com">Contact Us</a></div>
-                <div className="profile-button"><a href="purchase history">Purchases</a></div>
-                <div className="profile-button"><a href="Privacy Policy">Privacy Policy</a></div>
-                <div className="profile-button"><a href="Transaction History">Transactions </a></div>
-            </div>
+const Profile = () => {
+  
+ const [firstName, setFirstName] = useState('');
+ const [lastName, setLastName] = useState('');
+ const [mobileNumber, setMobileNumber] = useState('');
+ const [addressLine1, setAddressLine1] = useState('');
+ const [addressLine2, setAddressLine2] = useState('');
+ const [city, setCity] = useState('');
+ const [state, setState] = useState('');
+ const [pincode, setPincode] = useState('');
+ const [ownershipStatus, setOwnershipStatus] = useState('');
+ const [financeRequired, setFinanceRequired] = useState('');
+ const [dateofBirth, setdateofBirth] = useState('');
+ const [gender, setGender] = useState('');
 
+ const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted:', {
+      firstName,
+      lastName,
+      dateofBirth,
+      mobileNumber,
+      addressLine1,
+      addressLine2,
+      city,
+      state,
+      pincode,
+      ownershipStatus,
+      financeRequired,
+    });
+ };
+
+ return (
+  <div className='side-by-side'>
+    <h1 >USER PROFILE</h1>
+  
+    <form className="form" onSubmit={handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        type="text"
+        id="firstName"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        type="text"
+        id="lastName"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+
+      <label htmlFor="dateofBirth">Date of Birth</label>
+      <input
+        type="date"
+        id="dateofBirth"
+        value={dateofBirth}
+        onChange={(e) => setdateofBirth(e.target.value)}
+      />
+      <label htmlFor="gender">Gender</label>
+      {/* Gender radio button group */}
+      <div className="gender-radio-group">
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              checked={gender === 'male'}
+              onChange={() => setGender('male')}
+            />
+            Male
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              checked={gender === 'female'}
+              onChange={() => setGender('female')}
+            />
+            Female
+          </label>
         </div>
-        </Center>
-    )
+
+      <label htmlFor="mobileNumber">Mobile Number</label>
+      <input
+        type="text"
+        id="mobileNumber"
+        value={mobileNumber}
+        onChange={(e) => setMobileNumber(e.target.value)}
+      />
+
+      <label htmlFor="addressLine1">Address Line 1</label>
+      <input
+        type="text"
+        id="addressLine1"
+        value={addressLine1}
+        onChange={(e) => setAddressLine1(e.target.value)}
+      />
+
+      <label htmlFor="addressLine2">Address Line 2</label>
+      <input
+        type="text"
+        id="addressLine2"
+        value={addressLine2}
+        onChange={(e) => setAddressLine2(e.target.value)}
+      />
+
+      <label htmlFor="city">City</label>
+      <input
+        type="text"
+        id="city"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+
+      <label htmlFor="state">State</label>
+      <input
+        type="text"
+        id="state"
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+      />
+
+      <label htmlFor="pincode">Pincode</label>
+      <input
+        type="text"
+        id="pincode"
+        value={pincode}
+        onChange={(e) => setPincode(e.target.value)}
+      />
+
+      <label htmlFor="ownershipStatus">Ownership Status</label>
+      <input
+        type="text"
+        id="ownershipStatus"
+        value={ownershipStatus}
+        onChange={(e) => setOwnershipStatus(e.target.value)}
+      />
+
+      <label htmlFor="financeRequired">Finance Required</label>
+      <input
+        type="text"
+        id="financeRequired"
+        value={financeRequired}
+        onChange={(e) => setFinanceRequired(e.target.value)}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+ );
+};
 
 export default Profile;
