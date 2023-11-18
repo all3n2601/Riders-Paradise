16c8d5203263f381a5511628e1b589268846d4f6
rahulpalanivel <palanivelrahul45@gmail.com>
2023-11-18 11:10:13 +0530

    commit bike details

--- riders_paradise/src/components/data/BikeDetails.js
+++ riders_paradise/src/components/data/BikeDetails.js
@@ -3,6 +3,9 @@ import axios from "axios";
 import "../../styles/BikeDetails.css";
 import { useParams } from "react-router-dom";
 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
 function BikeDetails() {
   const id = useParams()._id;
   const [bike, setBike] = useState(null);
@@ -26,90 +29,114 @@ function BikeDetails() {
   if (!bike) {
     return <div>Loading...</div>; // Add a loading state
   }
-
+  const { images, otherBikeDetails } = bike;
   return (
-    <div className="body">
-      <div className="bike-info">
+    <div className="bodyy">
+      <section className="bikeimages">
         <div
           style={{
-            backgroundImage: `url(${bike.imageURL})`,
-            backgroundSize: "cover",
-            justifyContent: "center",
-            alignItems: "center",
+            position: "relative",
+            top: "70px",
+            left: "350px",
+            width: "120vh",
+            height: "80vh",
           }}
         >
-          <p className="bike-description">{bike.description}</p>
-          <div className="buttons">
-            <button className="button">Book a Test Ride</button>
-            <button className="button">Buy It</button>
-          </div>
+          <img
+            src={bike.imageURL}
+            alt="bike"
+            style={{ width: "900px", height: "600px" }}
+          />
+          {/* <div className="buttons">
+            <button className="button">Test Ride</button>
+            <button className="button">Pre-Book</button>
+          </div> */}
         </div>
-      </div>
-      <div className="bike-details">
-        <h2>
+
+        <h2 className="bike-header">
           {bike.brand} {bike.model} ({bike.year})
         </h2>
-        <table>
-          <tbody>
-            <tr>
-              <th>Type</th>
-              <td>{bike.type}</td>
-            </tr>
-            <tr>
-              <th>Frame Material</th>
-              <td>{bike.frameMaterial}</td>
-            </tr>
-            <tr>
-              <th>Frame Size</th>
-              <td>{bike.frameSize}</td>
-            </tr>
-            <tr>
-              <th>Color</th>
-              <td>{bike.color}</td>
-            </tr>
-            <tr>
-              <th>Price</th>
-              <td>${bike.price}</td>
-            </tr>
-            <tr>
-              <th>Frame Type</th>
-              <td>{bike.specifications.frameType}</td>
-            </tr>
-            <tr>
-              <th>Gear System</th>
-              <td>{bike.specifications.gearSystem}</td>
-            </tr>
-            <tr>
-              <th>Brakes</th>
-              <td>{bike.specifications.brakes}</td>
-            </tr>
-            <tr>
-              <th>Suspension</th>
-              <td>{bike.specifications.suspension}</td>
-            </tr>
-            <tr>
-              <th>Wheel Size</th>
-              <td>{bike.specifications.wheelSize}</td>
-            </tr>
-            <tr>
-              <th>Quantity Available</th>
-              <td>{bike.inventory.quantity}</td>
-            </tr>
-            <tr>
-              <th>Location</th>
-              <td>{bike.inventory.location}</td>
-            </tr>
-            <tr>
-              <th>Featured</th>
-              <td>{bike.isFeatured ? "Yes" : "No"}</td>
-            </tr>
-            <tr>
-              <th>Available</th>
-              <td>{bike.isAvailable ? "Yes" : "No"}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      </section>
+      <section className="bikeimages">
+        <div className="bike-details">
+          <table>
+            <tbody>
+              <tr>
+                <th>Type</th>
+                <td>{bike.type}</td>
+              </tr>
+              <tr>
+                <th>Frame Material</th>
+                <td>{bike.frameMaterial}</td>
+              </tr>
+              <tr>
+                <th>Frame Size</th>
+                <td>{bike.frameSize}</td>
+              </tr>
+              <tr>
+                <th>Color</th>
+                <td>{bike.color}</td>
+              </tr>
+              <tr>
+                <th>Price</th>
+                <td>${bike.price}</td>
+              </tr>
+              <tr>
+                <th>Frame Type</th>
+                <td>{bike.specifications.frameType}</td>
+              </tr>
+              <tr>
+                <th>Gear System</th>
+                <td>{bike.specifications.gearSystem}</td>
+              </tr>
+              <tr>
+                <th>Brakes</th>
+                <td>{bike.specifications.brakes}</td>
+              </tr>
+              <tr>
+                <th>Suspension</th>
+                <td>{bike.specifications.suspension}</td>
+              </tr>
+              <tr>
+                <th>Wheel Size</th>
+                <td>{bike.specifications.wheelSize}</td>
+              </tr>
+              <tr>
+                <th>Quantity Available</th>
+                <td>{bike.inventory.quantity}</td>
+              </tr>
+              <tr>
+                <th>Location</th>
+                <td>{bike.inventory.location}</td>
+              </tr>
+              <tr>
+                <th>Featured</th>
+                <td>{bike.isFeatured ? "Yes" : "No"}</td>
+              </tr>
+              <tr>
+                <th>Available</th>
+                <td>{bike.isAvailable ? "Yes" : "No"}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+      <section className="bikeimages">
+        <div className="bike-description">
+          <p className="desc">{bike.description}</p>
+        </div>
+      </section>
+      <section className="bikeimages">
+        {bike.images && bike.images.length > 0 && (
+          <Carousel>
+            {bike.images.map((images, index) => (
+              <div key={index}>
+                <img src={images} alt={`slide ${index + 1}`} />
+              </div>
+            ))}
+          </Carousel>
+        )}
+      </section>
     </div>
   );
 }
