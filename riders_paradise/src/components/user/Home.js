98cce4c40215fdfdcc48afc088275baacd933f58
Manitn55 <manigandan.21bce7922@vitapstudent.ac.in>
2023-11-20 20:20:53 +0530

    Home Page

--- riders_paradise/src/components/user/Home.js
+++ riders_paradise/src/components/user/Home.js
@@ -1,14 +1,53 @@
 import React from "react";
 import "../../styles/Home.css";
-import "../../assets/video.mp4";
+import img from "../../assets/bike.jpg"
+import video from "../../assets/video.mp4";
 import { Link } from "react-router-dom";
+import { Button } from "bootstrap";
 
 function Home() {
   return (
     <div class="body-homepage">
-      <video autoPlay loop>
-        <source src="../../assets/video.mp4" type="video/mp4" />
-      </video>
+      <div className="video-container">
+      <video src={video} autoPlay loop muted />
+      {/* <div className="btn-home">
+        <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">
+          Get Started
+        </Button>
+      </div> */}
+      <h1 className="h-home">Adventure Awaits</h1>
+      <p className="p-home">What are you waiting for??</p>
+      </div>
+      <div className="cards-home">
+        {/* <ul className="carditems-home">
+        <Link className="link-home">
+          <figure className="figure-home">
+            <img src={img} alt="" className="img-home"/>
+          </figure>
+        </Link>
+
+        <Link className="link-home">
+          <figure className="figure-home">
+            <img src={img} alt="" className="img-home"/>
+          </figure>
+        </Link>
+        </ul>
+
+        <ul className="carditems-home">
+        <Link className="link-home">
+          <figure className="figure-home">
+            <img src={img} alt="" className="img-home"/>
+          </figure>
+        </Link>
+
+        <Link className="link-home">
+          <figure className="figure-home">
+            <img src={img} alt="" className="img-home"/>
+          </figure>
+        </Link>
+        </ul> */}
+        
+      </div>
     </div>
   );
 }
